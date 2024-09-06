8acf2ce63b9f76ad62f9b02339d2b892279237d8
Ed <edward.hartnett@noaa.gov>
2024-09-06 11:09:19 -0600

    adding testing

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-from image_helpers import get_image_pixels, render_pixels
+from p1 import process_p1
 
 # Use the functions above to fetch pixel data and render the original data
-url = "https://www2.eecs.berkeley.edu/Faculty/Photos/Homepages/pamelafox.jpg"
-pixel_data = get_image_pixels(url)
-render_pixels(pixel_data)
+print(process_p1(3))
---
+++ tests/p1_test.py
@@ -0,0 +1,6 @@
+from p1 import process_p1
+
+
+def test_process_p1():
+    assert process_p1(2) == 3
+
