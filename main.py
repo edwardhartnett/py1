4c3a6aecc2a526f997a1e4882e05fc9df382a553
Ed <edward.hartnett@noaa.gov>
2024-09-06 10:37:38 -0600

    setup

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-from p1 import process_p1
+from image_helpers import get_image_pixels, render_pixels
 
-process_p1(2)
+# Use the functions above to fetch pixel data and render the original data
+url = "https://www2.eecs.berkeley.edu/Faculty/Photos/Homepages/pamelafox.jpg"
+pixel_data = get_image_pixels(url)
+render_pixels(pixel_data)
